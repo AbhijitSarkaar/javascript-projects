ae61c81a0441d1b57a0a6ee94d96f85ba63b5807
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2024-03-29 10:05:33 +0530

    expanding cards project done

--- expanding-cards/index.js
+++ expanding-cards/index.js
@@ -1,1 +1,22 @@
-console.log('expanding cards project')
+let cards = document.querySelectorAll('.card')
+
+cards.forEach(card => {
+    card.addEventListener('click', (event) => {
+
+        cards.forEach(singleCard => {
+            singleCard.classList.remove('active')
+        })
+
+        event.target.classList.add('active')
+    })
+})
+
+
+
+
+/*
+    - transition is used to define the transition between different states of an element, it can be applied on one or 
+    more properties of the element  
+    - element.classList.remove to remove a class
+    - element.classList.add to add a class
+*/
